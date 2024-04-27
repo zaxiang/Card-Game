6095f1ce8936ebed8905b1e45c7e261c2a76bc57
Zairan Xiang <zairan.xiang@duke.edu>
2024-04-27 17:09:50 -0400

    test if secrets.ts has been ignored

--- server/secrets.ts
+++ server/secrets.ts
@@ -4,7 +4,7 @@
 //     "gloas-6ca15fbb5d0db11494208a6b9ce90837b485bf6a19219a3f045487d0c99592dc",
 // };
 
-//for http://localhost:31000/login-callback, change to 30000 for kubernetes
+//for http://localhost:31000/login-callback, change to 31000 for kubernetes
 export const gitlab = {
   client_id: "9bf03f6602bbf47d2dec7eba3ae3ac24bd1784c59baec13cc882284a6abf38c9",
   client_secret:
